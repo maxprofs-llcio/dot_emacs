3bbb6179c4d3507e004560c5587c2f83deb5bdca
Kawano Kenichi <bppk28@gmail.com>
2019-05-26 01:42:32 +0900

    [update] markdown-mode

--- inits/61_markdown-mode.el
+++ inits/61_markdown-mode.el
@@ -7,11 +7,13 @@
 (setq auto-mode-alist (cons '("\\.markdown" . markdown-mode) auto-mode-alist))
 
 ;; Do not change font in code block
-(set-face-attribute 'markdown-code-face nil :inherit 'default)
-(set-face-attribute 'markdown-inline-code-face nil
-                    :inherit 'default
-                    :foreground (face-attribute font-lock-type-face :foreground))
-(setq markdown-fontify-code-blocks-natively t)
+(add-hook 'markdown-mode-hook
+          (lambda ()
+                 (set-face-attribute 'markdown-code-face nil :inherit 'default)
+                 (set-face-attribute 'markdown-inline-code-face nil
+                                     :inherit 'default
+                                     :foreground (face-attribute font-lock-type-face :foreground))
+                 (setq markdown-fontify-code-blocks-natively t)))
 
 (provide '61_markdowon-mode)
 ;;; 61_markdown-mode.el ends here
